7fa2587132a4eb44036b39e8c19f80844efd87b6
Leo1nardo1 <leonardo.carvalho.ferreira@protonmail.com>
2024-01-06 01:42:08 -0300

    PupFinder ofc

--- config/app.php
+++ config/app.php
@@ -6,13 +6,14 @@ use \Dotenv\Dotenv;
 
 
 
-define('DB_HOST', $_ENV['DB_HOST'] ?? '');
-define('DB_USER', $_ENV['DB_USER'] ?? '' );
-define('DB_PASSWORD', $_ENV['DB_PASSWORD'] ?? '' );
-define('DB_DATABASE', $_ENV['DB_DATABASE'] ?? '');
-define('DB_PORT', $_ENV['DB_PORT'] ?? '');
+define('DB_HOST', $_ENV['DB_HOST'] );
+define('DB_USER', $_ENV['DB_USER'] );
+define('DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+define('DB_DATABASE', $_ENV['DB_DATABASE'] );
+define('DB_PORT', $_ENV['DB_PORT']);
 
-define('SITE_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
+
+  define('SITE_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 
 
 
