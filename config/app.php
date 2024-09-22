e2c19d838fe2484102ada17b062dcdd0555376e5
Leo1nardo1 <leonardo.carvalho.ferreira@protonmail.com>
2024-09-22 00:54:48 -0300

    comments added bullet

--- config/app.php
+++ config/app.php
@@ -1,19 +1,17 @@
 <?php
 session_start();
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'pupfinder');
-define('DB_PORT', '3307');
+require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-//For hosting.
-// define('DB_HOST', $_ENV['DB_HOST'] );
-// define('DB_USER', $_ENV['DB_USER'] );
-// define('DB_PASSWORD', $_ENV['DB_PASSWORD'] );
-// define('DB_DATABASE', $_ENV['DB_DATABASE'] );
-// define('DB_PORT', $_ENV['DB_PORT']);
+define('DB_HOST', $_ENV['DB_HOST'] );
+define('DB_USER', $_ENV['DB_USER'] );
+define('DB_PASSWORD', $_ENV['DB_PASSWORD'] );
+define('DB_DATABASE', $_ENV['DB_DATABASE'] );
+define('DB_PORT', $_ENV['DB_PORT']);
 
+//hosting
 // define('SITE_URL', 'https://' . $_SERVER['HTTP_HOST'] . '/');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/pupfinder/');
 
